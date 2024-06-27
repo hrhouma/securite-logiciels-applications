1b285f0f913adb6eae185c19faecaa308bd46fe8
Haythem REHOUMA <rehoumahaythem@gmail.com>
2024-06-27 17:16:32 -0400

    Update aes_d.py

--- B4-Projet4/fichiers/aes_d.py
+++ B4-Projet4/fichiers/aes_d.py
@@ -1,40 +1,43 @@
 import base64
 import hashlib
-from Crypto.Cipher import AES
-from Crypto import Random
- 
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
 BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
- 
- 
+
+def pad(s):
+    pad_length = BLOCK_SIZE - len(s) % BLOCK_SIZE
+    return s + chr(pad_length) * pad_length
+
+def unpad(s):
+    return s[:-ord(s[-1])]
+
 def encrypt(raw, password):
     private_key = hashlib.sha256(password.encode("utf-8")).digest()
-    raw = pad(raw)
-    iv = Random.new().read(AES.block_size)
+    raw = pad(raw).encode('utf-8')
+    iv = get_random_bytes(AES.block_size)
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
- 
- 
+    return base64.b64encode(iv + cipher.encrypt(raw)).decode('utf-8')
+
 def decrypt(enc, password):
     private_key = hashlib.sha256(password.encode("utf-8")).digest()
     enc = base64.b64decode(enc)
-    iv = enc[:16]
+    iv = enc[:AES.block_size]
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(enc[16:]))
+    return unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
 
 op = input("Do you want to 1. Encrypt data or 2. Decrypt data?\n")
 if (op == '1'):
-	msg = input("Enter the message to be encrypted: ")
-	password = input("Enter password: ")
-	encrypted = encrypt(msg, password)
-	print(encrypted)
+    msg = input("Enter the message to be encrypted: ")
+    password = input("Enter password: ")
+    encrypted = encrypt(msg, password)
+    print("Encrypted:", encrypted)
 
 elif (op == '2'):
-	msg = input("Enter the message to be decrypted: ")
-	password = input("Enter password used for encryption: ")
-	decrypted = decrypt(msg, password)
-	print(bytes.decode(decrypted))
+    msg = input("Enter the message to be decrypted: ")
+    password = input("Enter password used for encryption: ")
+    decrypted = decrypt(msg, password)
+    print("Decrypted:", decrypted)
 
 else:
-	print("Invalid option!")
+    print("Invalid option!")
